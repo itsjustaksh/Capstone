60c67812fbbadd34b6e00aa0b5bb9050c858daf6
itsjustme7864 <aksh.ravishankar@gmail.com>
2022-11-18 12:35:01 -0500

    Setup of f'n and test data

--- TopDownConversion/topDown.py
+++ TopDownConversion/topDown.py
@@ -1,10 +1,24 @@
 import cv2 as cv
 import numpy as np
 
-def transform_perspective(img):
-    points = np.zeros((4,2), dtype=np.float16)
-    cv.getPerspectiveTransform()
+def locate_lane_points(image: cv.Mat):
+    points = np.zeros((4,2), dtype=np.float32)
+
+    
+    # Find 4 points on image to use as landmarks
+
+    return points
+
+def transform_perspective(imgPath: str):
+
+    # Var setup 
+    im = cv.imread(imgPath)
+    srcpoints = locate_lane_points(im)
+
+    # Apply Transformation
+    cv.getPerspectiveTransform(src=srcpoints, )
 
 
 if __name__ == "__main__":
+
     print("Start")
